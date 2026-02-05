3551b988a696922351397e1746b6e298ff6a3268
Purushotham <119475738+purushothamkalapu@users.noreply.github.com>
2026-02-05 20:37:05 +0530

    Intial commit of Multi LLM

---
+++ 04 Multiple LLM/src/test/java/com/purushotham/spring_openai_demo/SpringOpenaiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purushotham.spring_openai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenaiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
